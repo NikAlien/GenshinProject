e47dddd7f7ef5cb1d0d163adc322143888f7ca5b
NikAlien <115424518+NikAlien@users.noreply.github.com>
2024-04-04 14:39:32 +0300

    Pie-chart final touches

--- src/app/pie-chart/pie-chart.component.ts
+++ src/app/pie-chart/pie-chart.component.ts
@@ -27,9 +27,32 @@ export class PieChartComponent {
     datasets: [{
       type: 'pie',
       label: 'Vision',
-      data: []
+      data: [],
+      backgroundColor: ['#74C2A8', '#9FD6E3', '#A5C83B', '#AF8EC1', '#FAB632', '#4CC2F1', '#EF7938'],
     }]
   };
+
+  // barChartData: ChartData<'bar', {key: string, value: number} []> = {
+  //   labels: [
+  //     'Anemo',
+  //     'Cryo',
+  //     'Dendro',
+  //     'Electro',
+  //     'Geo',
+  //     'Hydro',
+  //     'Pyro'
+  //   ],
+  //   datasets: [{
+  //     type: 'bar',
+  //     label: 'Vision',
+  //     data: [],
+  //     backgroundColor: ['#74C2A8', '#9FD6E3', '#A5C83B', '#AF8EC1', '#FAB632', '#4CC2F1', '#EF7938'],
+  //     parsing: {
+  //       xAxisKey: 'key',
+  //       yAxisKey: 'value'
+  //     }
+  //   }]
+  // };
 
   constructor(private charaService : CharacterService) {}
   ngOnInit() : void {
@@ -55,6 +78,16 @@ export class PieChartComponent {
     this.pieChartData.datasets[0].data.push(
       {key: 'Pyro', value: this.charaService.filterCharactersVision('pyro').length}
     );
+
+    // this.barChartData.datasets[0].data.push(
+    //   {key: 'Anemo', value: this.charaService.filterCharactersVision('anemo').length}
+    // );
+    // this.barChartData.datasets[0].data.push(
+    //   {key: 'Cryo', value: this.charaService.filterCharactersVision('cryo').length}
+    // );
+    // this.barChartData.datasets[0].data.push(
+    //   {key: 'Dendro', value: this.charaService.filterCharactersVision('dendro').length}
+    // );
   }
 
 
