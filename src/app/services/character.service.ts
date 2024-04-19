241a3b7281808790ce7cee9f759d7b5838d91071
NikAlien <115424518+NikAlien@users.noreply.github.com>
2024-04-19 13:06:09 +0300

    Start for websockets

--- src/app/services/character.service.ts
+++ src/app/services/character.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Character } from '../character/character';
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { Stomp, CompatClient } from '@stomp/stompjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,7 @@ export class CharacterService {
   apiUrl = 'http://localhost:8080/api/v1/characters';
 
   constructor(private http: HttpClient) { 
+    // this.initUpdateSocket();
   }
 
   getCharacters(page: number, elements: number): Observable<Character[]> {
@@ -51,6 +53,7 @@ export class CharacterService {
   }
 
   getCharacter(id: number): Observable<Character>{
+    console.log('got character ' + id);
     return this.http.get<Character>(this.apiUrl + '/id_' + id);
   }
 
@@ -59,10 +62,56 @@ export class CharacterService {
   }
 
   updateCharacter(character: Character): Observable<number> {
-    return this.http.put<number>(this.apiUrl + '/update_' + character.id, character);
+    return this.http.put<number>(this.apiUrl + '/update_' + character.characterId, character);
   }
 
   deleteCharacter(id: number): Observable<boolean> {
     return this.http.delete<boolean>(this.apiUrl + '/delete_' + id);
   }
+
+
+
+  //      WebSocket update
+
+  // private stompClient: CompatClient = Stomp.client("ws://localhost:8080/messages");
+  // private callableMap: Map<any, Function> = new Map<any, Function>();
+  // private sizeLastUpdate: number = 8;
+
+  // private makeUpdateSocket() {
+    
+  //   this.stompClient.onConnect = () => {
+  //       this.stompClient.subscribe('/article', (result) => {
+  //         if (this.sizeLastUpdate < JSON.parse(result.body)) {
+  //           this.callableMap.forEach((value, key) => { value.call(key); })
+  //         }
+  //       });
+  //   };
+    
+  //   this.stompClient.onWebSocketError = (error) => {
+  //       console.error('Error with update websocket', error);
+  //   };
+    
+  //   this.stompClient.onStompError = (frame) => {
+  //       console.error('Broker reported error: ' + frame.headers['message']);
+  //       console.error('Additional details: ' + frame.body);
+  //   };
+
+  //   this.stompClient.activate();
+  // }
+  
+  // private requestUpdateSocket() {
+  //   if (this.stompClient == null) return;
+  //   this.stompClient.publish({
+  //     destination: "/app/article-time",
+  //     body: "{}"
+  //   });
+  // }
+
+  // private initUpdateSocket() {
+  //   this.makeUpdateSocket();
+  // }
+
+  // subscribeToSocket(self: any, callable: Function) {
+  //   this.callableMap.set(self, callable);
+  // }
 }
